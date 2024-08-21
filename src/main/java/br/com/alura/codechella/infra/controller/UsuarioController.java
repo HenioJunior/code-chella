d54ba0cef8670c16d594fde9ba0800c5d4da1c90
Henio Junior <henioajunior@gmail.com>
2024-08-21 11:40:58 -0300

    Criação do método listarUsuarios

--- src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
@@ -1,20 +1,25 @@
 package br.com.alura.codechella.infra.controller;
 
 import br.com.alura.codechella.application.usecases.CriarUsuario;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final CriarUsuario criarUsuario;
+    private final ListarUsuarios listarUsuarios;
 
-    public UsuarioController(CriarUsuario criarUsuario) {
+    public UsuarioController(CriarUsuario criarUsuario, ListarUsuarios listarUsuarios) {
         this.criarUsuario = criarUsuario;
+        this.listarUsuarios = listarUsuarios;
     }
+
 
     @PostMapping
     public UsuarioDto cadastrarUsuario(@RequestBody UsuarioDto dto) {
@@ -27,6 +32,8 @@ public class UsuarioController {
 
     @GetMapping
     public List<UsuarioDto> listarUsuarios() {
-        return null;
+        return listarUsuarios.obterTodosUsuarios().stream()
+                .map(u -> new UsuarioDto(u.getCpf(), u.getNome(), u.getNascimento(),
+                        u.getEmail())).collect(Collectors.toList());
     }
 }
